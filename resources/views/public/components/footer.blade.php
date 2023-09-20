67c003d85814f43a88fd4f30e5b4a7858203d1ec
muhammadroyhan <rayhanahsan14@gmail.com>
2023-09-20 15:11:21 +0700

    push

--- resources/views/public/components/footer.blade.php
+++ resources/views/public/components/footer.blade.php
@@ -3,14 +3,15 @@
         <div class="row gy-4">
             <div class="col-lg-2 align-self-center mb-4 mb-lg-0 text-center">
                 <a href="index.html" class="text-decoration-none">
-                    <img src="img/sipsb.png" width="125" height="35" class="img-fluid" alt="Footer Logo" />
+                    <img src="img/Logo Siresma_2-01.svg" width="125" height="35" class="img-fluid"
+                        alt="Footer Logo" />
                 </a>
                 <strong class="d-block text-color-dark">SIRESMA</strong>
             </div>
             <div class="col-lg-3">
                 <h4 class="font-weight-bold text-color-dark">About Us</h4>
                 <p class="text-3-5">
-                    <strong class="d-block text-color-dark">Sistem Informasi Pengelolaan Sampah Kecamatan Semarang Barat</strong>
+                    <strong class="d-block text-color-dark">Sistem Resik Mandiri Kelurahan Sambiroto Kecamatan Tembalang</strong>
                     Gedung Manggala Wanabakti, <br>
                     Blok IV Lantai 5.Jalan Gatot Subroto <br>
                     Jakarta 10270, Indonesia<br><br>
@@ -32,14 +33,21 @@
     <div class="footer-copyright bg-color-dark-scale-2">
         <div class="container py-2">
             <div class="row py-4">
-                <div class="col-md-4 d-flex align-items-center justify-content-center justify-content-md-start mb-2 mb-lg-0">
-                    <p class="text-color-grey text-3-5">Sistem Informasi Pengelolaan Sampah Kecamatan Semarang Barat © 2023. All Rights Reserved.</p>
+                <div
+                    class="col-md-4 d-flex align-items-center justify-content-center justify-content-md-start mb-2 mb-lg-0">
+                    <p class="text-color-grey text-3-5">Sistem Resik Mandiri Kelurahan Sambiroto Kecamatan Tembalang © 2023. All
+                        Rights Reserved.</p>
                 </div>
-                <div class="col-md-8 d-flex align-items-center justify-content-center justify-content-md-end mb-4 mb-lg-0">
-                    <ul class="footer-social-icons social-icons social-icons-clean social-icons-icon-light social-icons-big">
-                        <li class="social-icons-instagram"><a href="http://www.instagram.com/" target="_blank" title="Intagram"><i class="fab fa-instagram text-4"></i></a></li>
-                        <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter text-4"></i></a></li>
-                        <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f text-4"></i></a></li>
+                <div
+                    class="col-md-8 d-flex align-items-center justify-content-center justify-content-md-end mb-4 mb-lg-0">
+                    <ul
+                        class="footer-social-icons social-icons social-icons-clean social-icons-icon-light social-icons-big">
+                        <li class="social-icons-instagram"><a href="https://www.instagram.com/siresma.official/" target="_blank"
+                                title="Intagram"><i class="fab fa-instagram text-4"></i></a></li>
+                        <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank"
+                                title="Twitter"><i class="fab fa-twitter text-4"></i></a></li>
+                        <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank"
+                                title="Facebook"><i class="fab fa-facebook-f text-4"></i></a></li>
                     </ul>
                 </div>
             </div>
--- resources/views/public/components/header.blade.php
+++ resources/views/public/components/header.blade.php
@@ -1,4 +1,5 @@
-<header id="header" class="header-transparent header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
+<header id="header" class="header-transparent header-effect-shrink"
+    data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
     <div class="header-body border-top-0 bg-dark box-shadow-none">
         <div class="header-container container">
             <div class="header-row">
@@ -6,7 +7,7 @@
                     <div class="header-row">
                         <div class="header-logo text-center">
                             <a href="index.html">
-                                <img alt="SIPSB" width="50" height="50" src="img/siresma.png">
+                                <img alt="SIPSB" width="50" height="50" src="img/Logo Siresma_2-01.svg">
                                 <strong class="d-block text-color-light">SIRESMA</strong>
                             </a>
                         </div>
@@ -14,22 +15,24 @@
                 </div>
                 <div class="header-column justify-content-end">
                     <div class="header-row">
-                        <div class="header-nav header-nav-links header-nav-line header-nav-bottom-line header-nav-bottom-line-active-text-light header-nav-dropdowns-dark header-nav-light-text">
-                            <div class="header-nav-main header-nav-main-text-capitalize header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+                        <div
+                            class="header-nav header-nav-links header-nav-line header-nav-bottom-line header-nav-bottom-line-active-text-light header-nav-dropdowns-dark header-nav-light-text">
+                            <div
+                                class="header-nav-main header-nav-main-text-capitalize header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
                                 <nav class="collapse">
                                     <ul class="nav nav-pills" id="mainNav">
                                         <li>
-                                            <a class="nav-link active" href={{route('public.home')}}>
+                                            <a class="nav-link active" href={{ route('public.home') }}>
                                                 Beranda
                                             </a>
                                         </li>
                                         <li>
-                                            <a class="nav-link" href={{route('public.visionMission')}}>
+                                            <a class="nav-link" href={{ route('public.visionMission') }}>
                                                 Visi Misi
                                             </a>
                                         </li>
                                         <li>
-                                            <a class="nav-link" href={{route('public.newsIndex')}}>
+                                            <a class="nav-link" href={{ route('public.newsIndex') }}>
                                                 Berita
                                             </a>
                                         </li>
@@ -38,9 +41,12 @@
                                                 Data Pengelolaan Sampah
                                             </a>
                                             <ul class="dropdown-menu">
-                                                <li><a class="dropdown-item" href={{route('public.landfillData')}}>Timbunan Sampah</a></li>
+                                                <li><a class="dropdown-item"
+                                                        href={{ route('public.landfillData') }}>Timbunan Sampah</a></li>
                                                 {{-- <li><a class="dropdown-item" href="komposisi-sampah.html">Komposisi Sampah</a></li> --}}
-                                                <li><a class="dropdown-item" href={{route('public.wasteComposition')}}>Sumber Sampah</a></li>
+                                                <li><a class="dropdown-item"
+                                                        href={{ route('public.wasteComposition') }}>Sumber Sampah</a>
+                                                </li>
                                             </ul>
                                         </li>
                                         <li class="dropdown">
@@ -48,13 +54,15 @@
                                                 Fasilitas Pengelolaan Sampah
                                             </a>
                                             <ul class="dropdown-menu">
-                                                <li><a class="dropdown-item" href={{route('public.trashBank')}}>Bank Sampah</a></li>
+                                                <li><a class="dropdown-item" href={{ route('public.trashBank') }}>Bank
+                                                        Sampah</a></li>
                                                 {{-- <li><a class="dropdown-item" href="rumah-kompos.html">Rumah Kompos</a></li> --}}
-                                                <li><a class="dropdown-item" href={{route('public.tps3r')}}>TPS3R</a></li>
+                                                <li><a class="dropdown-item" href={{ route('public.tps3r') }}>TPS3R</a>
+                                                </li>
                                             </ul>
                                         </li>
                                         <li>
-                                            <a class="nav-link" href={{route('public.contactUs')}}>
+                                            <a class="nav-link" href={{ route('public.contactUs') }}>
                                                 Hubungi Kami
                                             </a>
                                         </li>
@@ -63,7 +71,8 @@
                             </div>
                         </div>
 
-                        <button class="btn header-btn-collapse-nav ms-3 ms-sm-4" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+                        <button class="btn header-btn-collapse-nav ms-3 ms-sm-4" data-bs-toggle="collapse"
+                            data-bs-target=".header-nav-main nav">
                             <i class="fas fa-bars"></i>
                         </button>
                     </div>
