c81cb1b08b4001798bed4d3b08cf68b72484ecff
IlhamNR <ilhamnurramadhan19@gmail.com>
2023-09-21 18:23:38 +0700

    dashboard layout update

--- resources/views/admin/components/layout.blade.php
+++ resources/views/admin/components/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Blank Page</title>
+    <title>Siresma Admin | Blank Page</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
@@ -19,7 +19,7 @@
 <body class="hold-transition sidebar-mini">
     <!-- Site wrapper -->
     <div class="wrapper">
- @include('admin.components.preloader')
+        @include('admin.components.preloader')
         @include('admin.components.navbar')
 
         @include('admin.components.sidebar')
