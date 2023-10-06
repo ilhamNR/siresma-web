727a14e9cc0a173b2879da2dc29d24e9711fa6be
IlhamNR <ilhamnurramadhan19@gmail.com>
2023-10-06 18:53:49 +0700

    mvoe dependencies to resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
 import './bootstrap';
-
+import $ from "jquery";
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import "/resources/public/vendor/modernizr/modernizr.min.js";
+import "/resources/js/admin/js/adminlte.min.js"
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
