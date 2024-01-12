891e98e712934c95db0bdcfac058a90abeb1cfd5
ilhamNR <ilhamnurramadhan19@gmail.com>
2024-01-12 15:47:48 +0700

    crud kategori sampah

--- app/Http/Controllers/Admin/TrashCategoryController.php
+++ app/Http/Controllers/Admin/TrashCategoryController.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\AppTrashCategory;
 use Yajra\DataTables\DataTables;
-
+use Illuminate\Support\Facades\Validator;
 class TrashCategoryController extends Controller
 {
+    // Fungsi untuk menampilkan data kategori sampah
     public function index(Request $request)
     {
         $category = AppTrashCategory::all();
@@ -16,5 +17,32 @@ class TrashCategoryController extends Controller
             return DataTables::of($category)->toJson();
         }
         return view('admin.trash-category.index');
+    }
+
+    // Fungsi untuk menambahkan kategori sampah baru
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'category_name' => 'required|string|max:255',
+            'unit' => 'required|string|max:50',
+            'price' => 'required|numeric|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        AppTrashCategory::create($request->all());
+
+        return response()->json(['message' => 'Kategori Sampah berhasil ditambahkan'], 200);
+    }
+
+    // Fungsi untuk menghapus kategori sampah
+    public function destroy($id)
+    {
+        $category = AppTrashCategory::findOrFail($id);
+        $category->delete();
+
+        return response()->json(['message' => 'Kategori Sampah berhasil dihapus'], 200);
     }
 }
